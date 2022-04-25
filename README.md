a021430ddf227ddbb4d8706c6444aca9cf6ebde7
jiangyi9 <jiangenin@gmail.com>
2022-04-25 11:33:40 +0200

    detect color

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-# Monitor
+# Introduction
+
+This is my Interdisciplinary project: to implement a monitoring system using camera footage and Lidars. I use Python as the main developing language.
+
+The goal of the monitoring system is to detect the position of goods (represented as cubes) in the context of a mock, Web of Things compliant industrial system, and propagate gained information to the system controller to allow for self-correcting behavior.
+
+The concrete tasks are: 
+
+- Research techniques and algorithms for detecting objects using image processing techniques and Lidars.
+- Getting familiar with the used machinery and devices.
+- Development and deployment of the monitoring system.
+- Stress testing the developed monitoring system.
+- Documentation of the system.
+
+
+
+## Access the video stream (not be tested yet)
+
+OpenCV provides a class to help us access video files, video streams, and cameras. We assume the RTSP is used to transmit the video, the example address is: rtsp://192.168.1.64/1. HTTP can also be used if required.
+
+```
+capture = cv2.VideoCapture('rtsp://192.168.1.64/1') 
+```
+
+For camera with IP address, we should add its username and password as parameters.
+
+```
+capture = cv2.VideoCapture('rtsp://[username]:
+[password]@192.168.1.64/1')
+```
+
+A full example to access the video stream is below. (see [here](https://cloud.tencent.com/developer/article/1697128))
+
+```
+import cv2
+
+#print("Before URL")
+cap = cv2.VideoCapture('rtsp://admin:123456@192.168.1.216/H264?ch=1&subtype=0')
+#print("After URL")
+
+while True:
+
+    #print('About to start the Read command')
+    ret, frame = cap.read()
+    #print('About to show frame of Video.')
+    cv2.imshow("Capturing",frame)
+    #print('Running..')
+
+    # press 'q' to break the 'while' loop. 
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release and destory the captured stream. This is important because it may cause significant system delay if someone doesn't destroy the window.
+cap.release()
+cv2.destroyAllWindows()
+```
+
+
+
+
+
+## Detect rectangles, identify their colors and locations
+
+We have read the video. Each frame of the video can be seen as a image.
+
+```
+cap = cv2.VideoCapture(0) # load the PC webcam
+ret, image = cap.read() # ret is a flag
+```
+
+
+
+
+
---
+++ src/main.py
@@ -0,0 +1,127 @@
+import cv2
+import numpy as np
+import sys
+import matplotlib as plt
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# hsv
+lower_red = np.array([156, 43, 46])       # 红色阈值下界
+higher_red = np.array([180, 255, 255])     # 红色阈值上界
+lower_yellow = np.array([15, 140, 210])   # 黄色阈值下界
+higher_yellow = np.array([35, 255, 255])  # 黄色阈值上界
+lower_blue = np.array([90,80,150])       # 蓝色阈值下界
+higher_blue = np.array([140,240,255])     # 蓝色阈值上界
+lower_green = np.array([35,43,46])      # 绿色阈值下界
+higher_green = np.array([77,255,255])     # 绿色阈值上界
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, image = cap.read()
+
+    # image = cv2.imread("shapes.png")
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # 默认是BRG，要转化成RGB，颜色才正常
+    image = cv2.resize(image, dsize=(648, 486))
+
+
+    img_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    mask_red = cv2.inRange(img_hsv, lower_red, higher_red)  # 可以认为是过滤出红色部分，获得红色的掩膜
+    mask_red = cv2.medianBlur(mask_red, 7)
+    mask_yellow = cv2.inRange(img_hsv, lower_yellow, higher_yellow)
+    mask_yellow = cv2.medianBlur(mask_yellow, 7)
+    mask_blue = cv2.inRange(img_hsv, lower_blue, higher_blue)
+    mask_blue = cv2.medianBlur(mask_blue, 7)
+    mask_green = cv2.inRange(img_hsv, lower_green, higher_green)
+    mask_green = cv2.medianBlur(mask_green, 7)
+
+    cnts1, hierarchy1 = cv2.findContours(mask_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # 轮廓检测 #红色
+    cnts2, hierarchy2 = cv2.findContours(mask_blue, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cnts3, hierarchy3 = cv2.findContours(mask_yellow, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cnts4, hierarchy4 = cv2.findContours(mask_green, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    image = image.copy()
+
+    for cnt in cnts1:#red                               b g r
+        (x, y, w, h) = cv2.boundingRect(cnt)  # 该函数返回矩阵四个点
+        rect = cv2.minAreaRect(cnt) # 得到最小外接矩形的（中心(x,y), (宽,高), 旋转角度）
+        area = cv2.contourArea(cnt)#获得blob的面积
+        if (area < 2000):#小于2000就跳过
+            continue
+        box = cv2.boxPoints(rect) #  获取最小外接矩形的4个顶点坐标
+        box = np.int0(box)#取整
+        cx = int(rect[0][0])#获取中心点x坐标
+        cy = int(rect[0][1])
+        # print(rect[2])#打印旋转角度
+        image = cv2.drawContours(image, [box], 0, (0, 0, 255), 3)#画旋转方框
+
+        #image = cv2.circle(image,(cx,cy),4,(0, 0, 255),3)
+        image = cv2.line(image,(cx+10,cy),(cx-10,cy),(0, 0, 255),2)
+        image = cv2.line(image,(cx,cy+10),(cx,cy-10),(0, 0, 255),2)
+        # cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)  # 将检测到的颜色框起来
+        cv2.putText(image, 'red', (x, y - 5), font, 0.7, (0, 0, 255), 2)
+
+    for cnt in cnts2:#blue
+        (x, y, w, h) = cv2.boundingRect(cnt)  # 该函数返回矩阵四个点
+        rect = cv2.minAreaRect(cnt) # 得到最小外接矩形的（中心(x,y), (宽,高), 旋转角度）
+        area = cv2.contourArea(cnt)#获得blob的面积
+        if (area < 2000):#小于200就跳过
+            continue
+        box = cv2.boxPoints(rect) #  获取最小外接矩形的4个顶点坐标
+        box = np.int0(box) #取整
+        cx = int(rect[0][0]) #获取中心点x坐标
+        cy = int(rect[0][1])
+        # print(rect[2]) #打印旋转角度
+        image = cv2.drawContours(image, [box], 0, (255, 0, 0), 3) #画旋转方框
+
+        #image = cv2.circle(image,(cx,cy),4,(0, 0, 255),3)
+        image = cv2.line(image,(cx+10,cy),(cx-10,cy),(255, 0, 0),2)
+        image = cv2.line(image,(cx,cy+10),(cx,cy-10),(255, 0, 0),2)
+        # cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)  # 将检测到的颜色框起来
+        cv2.putText(image, 'blue', (x, y - 5), font, 0.7, (255, 0, 0), 2)
+
+    for cnt in cnts3:#yellow
+        (x, y, w, h) = cv2.boundingRect(cnt)  # 该函数返回矩阵四个点
+        rect = cv2.minAreaRect(cnt) # 得到最小外接矩形的（中心(x,y), (宽,高), 旋转角度）
+        area = cv2.contourArea(cnt)#获得blob的面积
+        if (area < 2000):#小于2000就跳过
+            continue
+        box = cv2.boxPoints(rect) #  获取最小外接矩形的4个顶点坐标
+        box = np.int0(box)#取整
+        cx = int(rect[0][0])#获取中心点x坐标
+        cy = int(rect[0][1])
+        # print(rect[2])#打印旋转角度
+        image = cv2.drawContours(image, [box], 0, (0, 255, 255), 3)#画旋转方框
+
+        #image = cv2.circle(image,(cx,cy),4,(0, 0, 255),3)
+        image = cv2.line(image,(cx+10,cy),(cx-10,cy),(0, 255, 255),2)
+        image = cv2.line(image,(cx,cy+10),(cx,cy-10),(0, 255, 255),2)
+        # cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 255), 2)  # 将检测到的颜色框起来
+        cv2.putText(image, 'yellow', (x, y - 5), font, 0.5, (0, 255, 255), 2)
+
+    for cnt in cnts4:#green
+        (x, y, w, h) = cv2.boundingRect(cnt)  # 该函数返回矩阵四个点
+        rect = cv2.minAreaRect(cnt) # 得到最小外接矩形的（中心(x,y), (宽,高), 旋转角度）
+        area = cv2.contourArea(cnt)#获得blob的面积
+        if (area < 2000):#小于2000就跳过
+            continue
+        box = cv2.boxPoints(rect) #  获取最小外接矩形的4个顶点坐标
+        box = np.int0(box)#取整
+        cx = int(rect[0][0])#获取中心点x坐标
+        cy = int(rect[0][1])
+        # print(rect[2])#打印旋转角度
+        image = cv2.drawContours(image, [box], 0, (0, 255, 0), 3)#画旋转方框
+
+        # image = cv2.circle(image,(cx,cy),4,(0, 0, 255),3)
+        image = cv2.line(image,(cx+10,cy),(cx-10,cy),(0, 255, 0),2)
+        image = cv2.line(image,(cx,cy+10),(cx,cy-10),(0, 255, 0),2)
+        # cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)  # 将检测到的颜色框起来
+        cv2.putText(image, 'green', (x, y - 5), font, 0.7, (0, 255, 0), 2)
+
+    cv2.namedWindow("image", cv2.WINDOW_AUTOSIZE)
+    cv2.imshow("image", image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
+        break
