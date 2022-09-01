619a4d846e11c87ad26a0672252ed4fc232cedf2
jelly <jiangenin@gmail.com>
2022-09-01 19:01:29 +0200

    20220901

--- src/main_linux.py
+++ src/main_linux.py
@@ -1,4 +1,5 @@
 from pickle import TRUE
+from re import X
 import cv2
 import numpy as np
 
@@ -11,20 +12,68 @@ pic_undistorted_path = "/home/jelly/Desktop/Monitor/img_undistorted.png"
 pic_detected_path = "/home/jelly/Desktop/Monitor/image_detection.png"
 keyboard_flag = 0
 
+center_x = 900
+center_y = 700
+
+HIGH = 1
+LOW = 0
+
+corner_left_1_x = 198
+corner_left_1_y = 183
+corner_left_2_x = 320
+corner_left_2_y = 93
+corner_left_3_x = 940
+corner_left_3_y = 918
+corner_left_4_x = 812
+corner_left_4_y = 1014
+
+corner_right_1_x = 1765
+corner_right_1_y = 175
+corner_right_2_x = 1897
+corner_right_2_y = 230
+corner_right_3_x = 1627
+corner_right_3_y = 943
+corner_right_4_x = 1477
+corner_right_4_y = 880
+
+def isInterArea(testPoint):#testPoint为待测点[x,y]
+    LBPoint_1 = [corner_left_4_x, corner_left_4_y]
+    LTPoint_1 = [corner_left_1_x, corner_left_1_y]
+    RTPoint_1 = [corner_left_2_x, corner_left_2_y]
+    RBPoint_1 = [corner_left_3_x, corner_left_3_y]
+    a = (LTPoint_1[0]-LBPoint_1[0])*(testPoint[1]-LBPoint_1[1])-(LTPoint_1[1]-LBPoint_1[1])*(testPoint[0]-LBPoint_1[0])
+    b = (RTPoint_1[0]-LTPoint_1[0])*(testPoint[1]-LTPoint_1[1])-(RTPoint_1[1]-LTPoint_1[1])*(testPoint[0]-LTPoint_1[0])
+    c = (RBPoint_1[0]-RTPoint_1[0])*(testPoint[1]-RTPoint_1[1])-(RBPoint_1[1]-RTPoint_1[1])*(testPoint[0]-RTPoint_1[0])
+    d = (LBPoint_1[0]-RBPoint_1[0])*(testPoint[1]-RBPoint_1[1])-(LBPoint_1[1]-RBPoint_1[1])*(testPoint[0]-RBPoint_1[0])
+    in_area_1 = (a>0 and b>0 and c>0 and d>0) or (a<0 and b<0 and c<0 and d<0)
+
+    LBPoint_2 = [corner_right_4_x, corner_right_4_y]
+    LTPoint_2 = [corner_right_1_x, corner_right_1_y]
+    RTPoint_2 = [corner_right_2_x, corner_right_2_y]
+    RBPoint_2 = [corner_right_3_x, corner_right_3_y]
+    a = (LTPoint_2[0]-LBPoint_2[0])*(testPoint[1]-LBPoint_2[1])-(LTPoint_2[1]-LBPoint_2[1])*(testPoint[0]-LBPoint_2[0])
+    b = (RTPoint_2[0]-LTPoint_2[0])*(testPoint[1]-LTPoint_2[1])-(RTPoint_2[1]-LTPoint_2[1])*(testPoint[0]-LTPoint_2[0])
+    c = (RBPoint_2[0]-RTPoint_2[0])*(testPoint[1]-RTPoint_2[1])-(RBPoint_2[1]-RTPoint_2[1])*(testPoint[0]-RTPoint_2[0])
+    d = (LBPoint_2[0]-RBPoint_2[0])*(testPoint[1]-RBPoint_2[1])-(LBPoint_2[1]-RBPoint_2[1])*(testPoint[0]-RBPoint_2[0])
+    print(a,b,c,d)
+    in_area_2 = (a>0 and b>0 and c>0 and d>0) or (a<0 and b<0 and c<0 and d<0)
+
+    if in_area_1 or in_area_2:
+        return True
+    else:
+        return False
+
+
 def get_screenshot():
     pic = ImageGrab.grab()
     pic.save(pic_grab_path)
 
 
 def abc(x):
-    a = keyboard.KeyboardEvent('down', 28, 'enter')
-    #按键事件a为按下enter键，第二个参数如果不知道每个按键的值就随便写，
-    #如果想知道按键的值可以用hook绑定所有事件后，输出x.scan_code即可
     b = keyboard.KeyboardEvent('down', 25, 'p')
-    c = keyboard.KeyboardEvent('down', 16, 'q')
+
     
     if x.event_type == 'down' and x.name == b.name:
-        print("你按下了p键")
 
         # get_screenshot()
 
@@ -56,7 +105,15 @@ def undistort(frame):
     mapx, mapy = cv2.initUndistortRectifyMap(k, d, None, k, (w, h), 5)
     return cv2.remap(frame, mapx, mapy, cv2.INTER_LINEAR)
 
+def draw_points(image, x, y):
+    image = cv2.line(image,(x+3,y),(x-3,y),(0, 255, 0),10)
+    image = cv2.line(image,(x,y+3),(x,y-3),(0, 255, 0),10)
 
+def convert_array(point_list):
+    points = np.array(point_list)
+    points = points.reshape(-1,3)
+    points = points[np.lexsort(points[:,:-1:].T)] # sort according to the column location
+    return points
 
 
 def detect():
@@ -72,7 +129,6 @@ def detect():
     lower_green = np.array([35,43,46])      # define lower bound of "green"
     higher_green = np.array([77,255,255])   # define higher bound of "green"
 
-    cap = cv2.VideoCapture(0)
 
     point_list = [] # global point list
     yellow_point_list = [] # yellow point list
@@ -80,15 +136,10 @@ def detect():
     blue_point_list = [] # blue point list
     green_point_list = [] # green point list
 
-# while True:
-    # ret, image = cap.read()
 
     image = cv2.imread(pic_undistorted_path)
-    # image = ImageGrab.grab(bbox=(100, 100, 580, 400))
-    # image = np.array(image)
-    # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # transfer color space from BGR to RGB
-    # image = cv2.resize(image, dsize=(1000, 600)) # set the window in a fixed size (648, 486).
 
+    image = cv2.resize(image, dsize=(1920, 1080)) # set the window in a fixed size (1920, 1080).
 
     img_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     img_erode = cv2.erode(img_hsv, None, iterations=2)
@@ -121,8 +172,16 @@ def detect():
         cy = int(rect[0][1])
         point_list.append(cx)
         point_list.append(cy)
+        if(isInterArea([cx,cy])):
+            point_list.append(HIGH)
+        else:
+            point_list.append(LOW)
         red_point_list.append(cx)
         red_point_list.append(cy)
+        if(isInterArea([cx,cy])):
+            red_point_list.append(HIGH)
+        else:
+            red_point_list.append(LOW)
         # print(rect[2])#打印旋转角度
         # image = cv2.drawContours(image, [box], 0, (0, 0, 255), 3)#画旋转方框
 
@@ -144,8 +203,16 @@ def detect():
         cy = int(rect[0][1])
         point_list.append(cx)
         point_list.append(cy)
+        if(isInterArea([cx,cy])):
+            point_list.append(HIGH)
+        else:
+            point_list.append(LOW)
         blue_point_list.append(cx)
         blue_point_list.append(cy)
+        if(isInterArea([cx,cy])):
+            blue_point_list.append(HIGH)
+        else:
+            blue_point_list.append(LOW)
         # print(rect[2]) #打印旋转角度
         # image = cv2.drawContours(image, [box], 0, (255, 0, 0), 3) #画旋转方框
 
@@ -167,8 +234,16 @@ def detect():
         cy = int(rect[0][1])
         point_list.append(cx)
         point_list.append(cy)
+        if(isInterArea([cx,cy])):
+            point_list.append(HIGH)
+        else:
+            point_list.append(LOW)
         yellow_point_list.append(cx)
         yellow_point_list.append(cy)
+        if(isInterArea([cx,cy])):
+            yellow_point_list.append(HIGH)
+        else:
+            yellow_point_list.append(LOW)
         # print(rect[2])#打印旋转角度
 
         cnt_len = cv2.arcLength(cnt[0], True)
@@ -196,8 +271,16 @@ def detect():
         cy = int(rect[0][1])
         point_list.append(cx)
         point_list.append(cy)
+        if(isInterArea([cx,cy])):
+            point_list.append(HIGH)
+        else:
+            point_list.append(LOW)
         green_point_list.append(cx)
         green_point_list.append(cy)
+        if(isInterArea([cx,cy])):
+            green_point_list.append(HIGH)
+        else:
+            green_point_list.append(LOW)
         # print(cx, cy)
         # print(rect[2])#打印旋转角度
         # image = cv2.drawContours(image, [box], 0, (0, 255, 0), 3)#画旋转方框
@@ -208,24 +291,47 @@ def detect():
         # cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)  # 将检测到的颜色框起来
         cv2.putText(image, 'green', (x, y - 5), font, 0.7, (0, 255, 0), 2)
 
-    # cv2.namedWindow("image", cv2.WINDOW_AUTOSIZE)
-    # cv2.imshow("image", image)
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     # cap.release()
-    #     cv2.destroyAllWindows()
-    #     break
+######################     draw some lines/points to help debug STARTs    #####################
+
+    draw_points(image, center_x, center_y)
+    draw_points(image, corner_left_1_x, corner_left_1_y)
+    draw_points(image, corner_left_2_x, corner_left_2_y)
+    draw_points(image, corner_left_3_x, corner_left_3_y)
+    draw_points(image, corner_left_4_x, corner_left_4_y)
+    draw_points(image, corner_right_1_x, corner_right_1_y)
+    draw_points(image, corner_right_2_x, corner_right_2_y)
+    draw_points(image, corner_right_3_x, corner_right_3_y)
+    draw_points(image, corner_right_4_x, corner_right_4_y)
+
+    pts_1 = np.array([[corner_left_4_x, corner_left_4_y],[corner_left_1_x, corner_left_1_y],[corner_left_2_x, corner_left_2_y],[corner_left_3_x, corner_left_3_y]], np.int32)
+    pts_1 = pts_1.reshape((-1, 1, 2))
+    zeros = np.zeros((image.shape), dtype=np.uint8)
+    mask_1 = cv2.fillConvexPoly(zeros, pts_1, (255,0,255))
+
+    pts_2 = np.array([[corner_right_4_x, corner_right_4_y],[corner_right_1_x, corner_right_1_y],[corner_right_2_x, corner_right_2_y],[corner_right_3_x, corner_right_3_y]], np.int32)
+    pts_2 = pts_2.reshape((-1, 1, 2))
+    mask_2 = cv2.fillConvexPoly(zeros, pts_2, (255,0,255))
+    image = 0.3*mask_1 + 0.3*mask_2 + image
+   
     cv2.imwrite(pic_detected_path, image)
-    point_array = np.array(point_list)
-    point_array = point_array.reshape(-1,2)
-    red_point_array = np.array(red_point_list)
-    red_point_array = red_point_array.reshape(-1,2)
-    blue_point_array = np.array(blue_point_list)
-    blue_point_array = blue_point_array.reshape(-1,2)
-    yellow_point_array = np.array(yellow_point_list)
-    yellow_point_array = yellow_point_array.reshape(-1,2)
-    green_point_array = np.array(green_point_list)
-    green_point_array = green_point_array.reshape(-1,2)
-    print(green_point_array[np.lexsort(green_point_array.T)])
+
+######################     draw some lines/points to help debug ENDs     #####################
+
+
+
+
+    point_array = convert_array(point_list)
+    red_point_array = convert_array(red_point_list)
+    blue_point_array = convert_array(blue_point_list)
+    yellow_point_array = convert_array(yellow_point_list)
+    green_point_array = convert_array(green_point_list)
+
+    # print(green_point_array)
+    # for x in green_point_array:
+    #     print(x[1])
+
+    # print(isInterArea([1700,600]))
+    print(point_array)
 
 
 if __name__=="__main__":  
